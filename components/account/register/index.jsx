239abef4e9d50c7b3e4d1e3bc3d76bf9b1f28289
mahesh-chand <maheshpk678@gmail.com>
2023-12-02 18:35:34 +0530

    register

---
+++ components/account/register/index.jsx
@@ -0,0 +1,13 @@
+import Layout from "../../shared/layout";
+
+const Register = () => {
+    return (
+        <Layout>
+            <div>
+gfffff
+            </div>
+        </Layout>
+    )
+}
+
+export default Register;
